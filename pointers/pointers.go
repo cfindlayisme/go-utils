eda816953c370180bbef243a78e10d429d7dc0f5
Chuck Findlay <chuck@findlayis.me>
2024-12-06 21:48:42 -0400

    add float, complex64/128, and byte pointer functions

--- pointers/pointers.go
+++ pointers/pointers.go
@@ -15,3 +15,19 @@ func IntPtr(i int) *int {
 func Float64Ptr(f float64) *float64 {
 	return &f
 }
+
+func Float32Ptr(f float32) *float32 {
+	return &f
+}
+
+func Complex64Ptr(c complex64) *complex64 {
+	return &c
+}
+
+func Complex128Ptr(c complex128) *complex128 {
+	return &c
+}
+
+func BytePtr(b byte) *byte {
+	return &b
+}
